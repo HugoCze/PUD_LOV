4ab9cb0a60a78db712eb03fd2a3b952d917c7cdc
Czerniawski <czerniah@emea.roche.com>
2022-12-26 13:56:04 +0100

    just a update

--- test2.py
+++ test2.py
@@ -20,30 +20,5 @@ chrome_options.add_argument("--allow-insecure-localhost")
 
 chrome_options.headless = False
 driver = webdriver.Chrome(options=chrome_options)
-# win
-# PATH = r"C:\Users\Hugo\PartyBot\chromedriver.exe"
-# driver = webdriver.Chrome(PATH, options=chrome_options)
 
-ignored_exceptions=(NoSuchElementException,StaleElementReferenceException,ElementNotInteractableException, TimeoutException, ElementClickInterceptedException)
-wait = WebDriverWait(driver, 10, ignored_exceptions=ignored_exceptions)
-
-print("start")
-start_time = time.time()
 driver.get("https://www.pudelek.pl/robert-lewandowski-jednak-komentuje-30-milionow-premii-od-morawieckiego-to-sie-wszystko-dzieje-poza-pilkarzami-ktorzy-razem-z-kibicami-sa-ofiarami-6842487736318496a")
-TermsAndConditions = wait.until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[3]/div/div[2]/div[3]/div/button[2]")))
-TermsAndConditions.click()
-print("Terms accepted")
-
-h1 = driver.find_element(By.TAG_NAME, 'h1')
-h1_text = h1.get_attribute('innerHTML')
-print(h1_text)
-
-
-end_time = time.time()
-total_time = end_time - start_time
-print("Time of looking the comment is equal to: ", total_time)
-
-# Test notes: 
-
-# on pc
-# Time of looking the comment is equal to:  1.8768177032470703
